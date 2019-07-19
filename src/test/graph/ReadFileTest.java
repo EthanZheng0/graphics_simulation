dc637000d2643673985e42bfdb25d08efcd64678
zhengyishan <y.zheng@wustl.edu>
2019-07-19 00:33:27 -0500

    speed up drawing

--- src/test/graph/ReadFileTest.java
+++ src/test/graph/ReadFileTest.java
@@ -2,13 +2,17 @@ package test.graph;
 
 import java.awt.Color;
 
+import edu.princeton.cs.introcs.StdDraw;
 import support.graph.Skeleton;
 
 public class ReadFileTest {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		Skeleton s = Skeleton.loadSkeletonFromFile();
 		s.drawSkeleton(Color.RED, Color.BLACK, 0.001, 0.001, true, false);
+		
+		StdDraw.show(500);
 	}
 
 }
